3629dfc3aef6fa6765775a92358f074feb3b4576
Alex Kh <khodos.alexey@gmail.com>
2016-09-06 23:37:11 -0300

    fix scrolling

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -1,13 +1,40 @@
 $(document).on('turbolinks:load', function() {
   submitNewMessage();
+  scrDown();
+});
+
+$(document).on('turbolinks:load', function() {
+  $('#send_message_chatroom').on("click", function(){
+    $('[data-send="message"]').click();
+    afterSend();
+    return false;
+  });
 });
 
 function submitNewMessage(){
   $('textarea#message_content').keydown(function(event) {
     if (event.keyCode == 13) {
         $('[data-send="message"]').click();
-        $('[data-textarea="message"]').val(" ")
+        afterSend();
         return false;
      }
+  });
+}
+
+function afterSend() {
+  $('[data-textarea="message"]').val(" ");
+  $('[data-textarea="message"]').focus();
+  $('#chatroom-card').scrollTop($('#chatroom-card')[0].scrollHeight);
+}
+
+function scrDown(){
+  $("#chatroom-card").hover(function(){
+    $(this).stop();
+    try { clearInterval(scroller); } catch(e) {}
+  },
+  function(){
+    scroller=setInterval(function(){
+      $('#chatroom-card').scrollTop($('#chatroom-card')[0].scrollHeight);
+    }, 1500);
   });
 }
